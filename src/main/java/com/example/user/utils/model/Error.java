955f2ff2624cae2779cc4e62e142179801837a02
aiza-mansur16 <aiza.mansur96@gmail.com>
2024-09-25 19:33:19 +0500

    spotbugs configuration

--- src/main/java/com/example/user/utils/model/Error.java
+++ src/main/java/com/example/user/utils/model/Error.java
@@ -9,6 +9,10 @@ import java.util.List;
 
 @Getter
 @Setter
+@edu.umd.cs.findbugs.annotations.SuppressFBWarnings(value = {
+        "EI_EXPOSE_REP",
+        "EI_EXPOSE_REP2"
+}, justification = "Error is used for error does not expose any sensitive information.")
 public class Error {
         @Schema(description = "status code of the error")
         private HttpStatus statusCode;
